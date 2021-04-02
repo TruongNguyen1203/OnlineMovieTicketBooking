9b3a9cb0950acd97b2bf727a31406036c1268941
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2021-04-02 10:09:20 +0700

    Set up entityframework core

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+            
+        }
+    }
+}
