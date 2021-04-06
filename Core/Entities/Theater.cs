5341581664b357e46aa0034b44c2d69aad97a79b
Thuyety <thuyety15@gmail.com>
2021-04-06 23:36:23 +0700

    create object and controller

---
+++ Core/Entities/Theater.cs
@@ -0,0 +1,10 @@
+namespace Core.Entities
+{
+    public class Theater
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Hotline { get; set; }
+    }
+}
